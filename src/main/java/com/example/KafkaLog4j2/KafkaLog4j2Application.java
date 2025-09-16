723a7eb504b205ea92e8a8dcd2a904c7b7dbf2c9
PavelSchelkin <schelkinpm@gmail.com>
2025-09-16 12:49:07 +0300

    init

--- src/main/java/com/example/KafkaLog4j2/KafkaLog4j2Application.java
+++ src/main/java/com/example/KafkaLog4j2/KafkaLog4j2Application.java
@@ -1,13 +1,13 @@
-package com.example.KafkaLog4j2;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class KafkaLog4j2Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaLog4j2Application.class, args);
-	}
-
-}
+//package com.example.KafkaLog4j2;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class KafkaLog4j2Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(KafkaLog4j2Application.class, args);
+//	}
+//
+//}
